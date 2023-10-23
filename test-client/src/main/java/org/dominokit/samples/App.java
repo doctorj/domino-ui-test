d48a22dd5976aff9408d822c2dd7b4f8c4882eca
doctorj <>
2023-10-23 10:37:38 +0200

    init

--- test-client/src/main/java/org/dominokit/samples/App.java
+++ test-client/src/main/java/org/dominokit/samples/App.java
@@ -1,7 +1,11 @@
 package org.dominokit.samples;
 
+import org.dominokit.domino.ui.button.Button;
 import org.dominokit.domino.ui.elements.DivElement;
 import org.dominokit.domino.ui.layout.AppLayout;
+import org.dominokit.domino.ui.menu.Menu;
+import org.dominokit.domino.ui.menu.MenuItem;
+import org.dominokit.domino.ui.menu.direction.DropDirection;
 import org.dominokit.domino.ui.style.DominoCss;
 import org.dominokit.domino.ui.utils.ElementsFactory;
 
@@ -12,7 +16,27 @@ public class App implements EntryPoint, ElementsFactory, DominoCss {
     public void onModuleLoad() {
         var layout = AppLayout.create("Domino-ui starter");
 
-        // layout.getContent().appendChild();
+		Button b1 = Button.create("Button")
+						.setDropMenu(Menu.<String>create()
+										.setDropDirection(DropDirection.BOTTOM_RIGHT)
+										.appendChild(MenuItem.create("Item 1"))
+										.appendChild(MenuItem.create("Item 2"))
+						)
+						;
+		DivElement d1 = div().appendChild(b1);
+
+		DivElement p = div();
+		p.appendChild(div().addCss(dui_flex, dui_gap_1, dui_m_b_10)
+							.appendChild(Button.create("Remove")
+											.addClickListener(evt -> d1.remove())
+							)
+							.appendChild(Button.create("Add")
+											.addClickListener(evt -> layout.getContent().appendChild(d1))
+							)
+		);
+
+		layout.getContent().appendChild(p);
+		layout.getContent().appendChild(d1);
 
         body().appendChild(layout);
     }
